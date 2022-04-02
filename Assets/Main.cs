d4d18dfedb8b48488df1c0331a10e71cb6bd0964
AdlanSADOU <adlsadou@gmail.com>
2022-04-02 21:58:37 +0200

    time bomd

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Main : MonoBehaviour
 {
 
     GameObject entity_prefab;
     GameObject torch;
+    GameObject canvas;
+    GameObject timer_text;
 
     class Entity
     {
@@ -18,33 +21,73 @@ public class Main : MonoBehaviour
     };
     List<Entity> entities;
 
+    public float time_scale = 1;
+    float countdown = 3;
+
     void Start()
     {
+        canvas = GameObject.Find("Canvas");
+        timer_text = GameObject.Find("Timer");
+        print(timer_text);
+        timer_text.GetComponent<Text>().text = countdown.ToString();
+
+
+
+
+
         torch = GameObject.Find("Torch");
         entity_prefab = Resources.Load<GameObject>("Prefabs/Entity");
+
+
+
 
         entities = new List<Entity>();
 
         for (int i = 0; i < 300; i++)
         {
-            Entity e = new Entity();
-            e.gameObject = Instantiate<GameObject>(entity_prefab, new Vector3(0, 0, 0), Quaternion.identity);
-
             float r_x = Random.Range(40, 100);
             float r_y = Random.Range(40, 100);
+            Entity e = new Entity();
+            Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(r_x, r_y, 0));
+            pos.z = 0;
+            e.gameObject = Instantiate<GameObject>(entity_prefab, pos, Quaternion.identity);
+
             e.speed = 16;
 
             e.direction = new Vector2(r_x, r_y);
             e.direction.Normalize();
-            print(e.direction);
             entities.Add(e);
         }
     }
 
     public float spot_z = -2f;
 
+    public float elapsed_time = 0;
+    bool first = true;
+
+    int i;
     void Update()
     {
+        print(countdown);
+
+        elapsed_time += Time.deltaTime;
+        if (elapsed_time >= 1 && countdown > 0)
+        {
+            countdown -= 1;
+            timer_text.GetComponent<Text>().text = countdown.ToString();
+            elapsed_time = 0;
+        }
+
+        if (first && countdown <= 0)
+        {
+            print(first);
+            Time.timeScale = 1;
+            canvas.transform.GetChild(0).gameObject.SetActive(false);
+            first = false;
+        }
+
+        if (first) return;
+
         ///////////////////////////////
         // Torch
         Vector3 mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -59,7 +102,6 @@ public class Main : MonoBehaviour
 
 
 
-
         ///////////////////////////////
         // Entities
         for (int i = 0; i < entities.Count; i++)
@@ -68,12 +110,12 @@ public class Main : MonoBehaviour
             Vector3 entity_pos = entities[i].gameObject.transform.position;
 
             // print("wToS: "+Camera.main.WorldToScreenPoint(entity_pos).x);
-            if (((Camera.main.WorldToScreenPoint(entity_pos).x >= Screen.width) || (Camera.main.WorldToScreenPoint(entity_pos).x <= 0))
+            if (((Camera.main.WorldToScreenPoint(entity_pos).x > Screen.width) || (Camera.main.WorldToScreenPoint(entity_pos).x < 0))
             )
             {
                 entities[i].direction.x = (entities[i].direction.x) * -1;
             }
-            if (((Camera.main.WorldToScreenPoint(entity_pos).y >= Screen.height) || (Camera.main.WorldToScreenPoint(entity_pos).y <= 0)))
+            if (((Camera.main.WorldToScreenPoint(entity_pos).y > Screen.height) || (Camera.main.WorldToScreenPoint(entity_pos).y < 0)))
             {
                 entities[i].direction.y = (entities[i].direction.y) * -1;
             }
